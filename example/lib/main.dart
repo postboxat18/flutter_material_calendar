accd0dc9578379d0196f217bc6d9946f97c297c0
postboxat18 <aravinthc18@gmail.com>
2025-03-01 21:01:24 +0530

    updated readme

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -34,65 +34,65 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   late List<EventList> eventList = [
-    EventList("2024-03-01", {
-      "2024-03-01": [EventName(Color(0xff008A5E), "10:20", "10:20")]
+    EventList("2025-03-01", {
+      "2025-03-01": [EventName(Color(0xff008A5E), "10:20", "10:20")]
     }),
-    EventList("2024-03-03", {
-      "2024-03-03": [EventName(Color(0xffF1BE41), "PH", "Public Holiday")]
+    EventList("2025-03-03", {
+      "2025-03-03": [EventName(Color(0xffF1BE41), "PH", "Public Holiday")]
     }),
-    EventList("2024-03-05", {
-      "2024-03-05": [
+    EventList("2025-03-05", {
+      "2025-03-05": [
         EventName(Color(0xff008A5E), "10:20", "10:20"),
         EventName(Color(0xffB73434), "SL", "Sick Leave")
       ]
     }),
-    EventList("2024-03-06", {
-      "2024-03-06": [EventName(Color(0xff008A5E), "10:20", "10:20")]
+    EventList("2025-03-06", {
+      "2025-03-06": [EventName(Color(0xff008A5E), "10:20", "10:20")]
     }),
-    EventList("2024-03-07", {
-      "2024-03-07": [EventName(Color(0xffB73434), "CL", "Casual Leave")]
+    EventList("2025-03-07", {
+      "2025-03-07": [EventName(Color(0xffB73434), "CL", "Casual Leave")]
     }),
-    EventList("2024-03-10", {
-      "2024-03-10": [EventName(Color(0xffF1BE41), "PH", "Public Holiday")]
+    EventList("2025-03-10", {
+      "2025-03-10": [EventName(Color(0xffF1BE41), "PH", "Public Holiday")]
     }),
-    EventList("2024-03-11", {
-      "2024-03-11": [
+    EventList("2025-03-11", {
+      "2025-03-11": [
         EventName(Color(0xff008A5E), "10:20", "10:20"),
         EventName(Color(0xffB73434), "CL", "Casual Leave")
       ]
     }),
-    EventList("2024-03-14", {
-      "2024-03-14": [EventName(Color(0xffF1BE41), "CL", "Casual Leave")]
+    EventList("2025-03-14", {
+      "2025-03-14": [EventName(Color(0xffF1BE41), "CL", "Casual Leave")]
     }),
-    EventList("2024-03-15", {
-      "2024-03-15": [EventName(Color(0xffF1BE41), "PH", "Public Holiday")]
+    EventList("2025-03-15", {
+      "2025-03-15": [EventName(Color(0xffF1BE41), "PH", "Public Holiday")]
     }),
-    EventList("2024-03-16", {
-      "2024-03-16": [EventName(Color(0xff008A5E), "10:20", "10:20")]
+    EventList("2025-03-16", {
+      "2025-03-16": [EventName(Color(0xff008A5E), "10:20", "10:20")]
     }),
-    EventList("2024-03-18", {
-      "2024-03-18": [
+    EventList("2025-03-18", {
+      "2025-03-18": [
         EventName(Color(0xff008A5E), "10:20", "10:20"),
         EventName(Color(0xffB73434), "SL", "Sick Leave")
       ]
     }),
-    EventList("2024-03-20", {
-      "2024-03-20": [EventName(Color(0xff008A5E), "10:20", "10:20")]
+    EventList("2025-03-20", {
+      "2025-03-20": [EventName(Color(0xff008A5E), "10:20", "10:20")]
     }),
-    EventList("2024-03-22", {
-      "2024-03-22": [EventName(Color(0xffB73434), "SL", "Sick Leave")]
+    EventList("2025-03-22", {
+      "2025-03-22": [EventName(Color(0xffB73434), "SL", "Sick Leave")]
     }),
-    EventList("2024-03-24", {
-      "2024-03-24": [EventName(Color(0xff008A5E), "10:20", "10:20")]
+    EventList("2025-03-24", {
+      "2025-03-24": [EventName(Color(0xff008A5E), "10:20", "10:20")]
     }),
-    EventList("2024-03-26", {
-      "2024-03-26": [EventName(Color(0xffB73434), "CL", "Casual Leave")]
+    EventList("2025-03-26", {
+      "2025-03-26": [EventName(Color(0xffB73434), "CL", "Casual Leave")]
     }),
-    EventList("2024-03-29", {
-      "2024-03-29": [EventName(Color(0xff008A5E), "10:20", "10:20")]
+    EventList("2025-03-29", {
+      "2025-03-29": [EventName(Color(0xff008A5E), "10:20", "10:20")]
     }),
-    EventList("2024-03-30", {
-      "2024-03-30": [EventName(Color(0xffF1BE41), "SL", "Sick Leave")]
+    EventList("2025-03-30", {
+      "2025-03-30": [EventName(Color(0xffF1BE41), "SL", "Sick Leave")]
     }),
   ];
 
